eac2f0e8984fe57ed95edfcc0b2cab1f18cfd9f3
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-23 11:11:18 -0300

    instantiating student controller, getting data and calling add method

--- app/app.ts
+++ app/app.ts
@@ -8,3 +8,15 @@ table.classList.add("alunos-table");
 mensagem.classList.add("mensagemView");
 
 body.appendChild(table);
+
+const form = document.querySelector(".form");
+
+//o método before adiciona um elemento antes de outro elemento
+form.before(mensagem);
+
+const alunoController = new AlunoController();
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alunoController.adiciona();
+});
